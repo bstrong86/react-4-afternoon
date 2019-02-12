661b8d109264a235e66548eb03edb4d9b5d1dd5a
Ben Strong <whicker86@gmail.com>
2019-02-12 16:41:09 -0700

    finished react-4-afternoon

--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
-
+import axios from 'axios'
+import {Route, Switch, Link} from 'react-router-dom'
+import ClassList from '../ClassList/ClassList'
 export default class Student extends Component {
   constructor() {
     super()
+
+    this.state = {
+      studentInfo: []
+    }
+
+  }
+    componentDidMount(){
+      return axios.get(`http://localhost:3005/students/${this.props.match.params.id}`).then(results =>
+        this.setState({
+          studentInfo: results.data
+        })
+      )
+
 
   }
 
@@ -10,7 +25,13 @@ export default class Student extends Component {
     return (
       <div className="box">
         <h1>Student</h1>
+        <h1>{this.state.studentInfo.first_name} {this.state.studentInfo.last_name}</h1>
+        <h3>Grade: {this.state.studentInfo.grade}</h3>
+        <h3>Email: {this.state.studentInfo.email}</h3>
+        <Link to={`/classlist/${this.state.studentInfo.class}`}> <button>ClassList</button></Link>
+        <Route component={ClassList} exact path='/classlist/:class'></Route>
       </div>
+
     )
   }
 }
